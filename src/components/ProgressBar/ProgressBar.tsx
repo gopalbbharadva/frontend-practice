136dc824c4701cc21798264dedb06bc2bb5ec5d3
gopalbbharadva <gopalbbharadva@gmail.com>
2023-09-26 00:01:26 +0530

    Refactor: removed the extra state for maintaining completed state of progress bar completion.

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,16 +2,13 @@ import { useEffect, useState } from 'react'
 
 export const ProgressBar = () => {
   const [progressValue, setProgressValue] = useState(0)
-  const [isCompleted, setIsCompleted] = useState(false)
 
   useEffect(() => {
     const timer = setInterval(
       () => setProgressValue((prev) => (prev < 100 ? prev + 1 : prev)),
       100
     )
-    if (progressValue === 100) {
-      setIsCompleted((prev) => !prev)
-    }
+
     // TODO: need to find for explore how useEffect will work with setInterval
     return () => clearInterval(timer)
   }, [progressValue])
@@ -23,7 +20,7 @@ export const ProgressBar = () => {
         <span>{progressValue}%</span>
         <div style={{ width: `${progressValue}%` }} />
       </div>
-      <p>{isCompleted ? 'Completed' : 'Loading...'}</p>
+      <p>{progressValue === 100 ? 'Completed' : 'Loading...'}</p>
     </div>
   )
 }
